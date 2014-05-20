a9b709a6a45d867b83e47c0198a2ce08e58561a0
wilfredwang <scorpio147wbh@gmail.com>
2014-05-20 22:16:56 +0800

    phonejs localStorage, autoPaging, pullDown

--- views/info.js
+++ views/info.js
@@ -1,39 +1,50 @@
-﻿HandyHit.data.feedEntries = ko.observableArray();
+﻿HandyHit.data.feedEntrySource = new DevExpress.data.DataSource({
+    store: {
+        type: 'local',
+        name: 'feedEntries',
+        key: 'publishedDate',
+        flushInterval: 2000
+    },
+    pageSize: 10,
+    sort: {
+        getter: 'publishedDate',
+        desc: true
+    }
+});
 HandyHit.info = function (params) {
-    var isLoaded = ko.observable(false),
-        loadPanelVisible = ko.observable(false),
-        loadPanelMessage = ko.observable();
+    var autoPagingEnabled = ko.observable(true);
+    var showNextButton = ko.observable(false);
+    var pullRefreshEnabled = ko.observable(true);
+    var pullingDownText = ko.observable('往下拉更新资讯');
+    var pulledDownText = ko.observable('可以松开了');
 
-    function loadData() {
-        isLoaded(false);
-        loadPanelVisible(true);
-        loadPanelMessage('正在加载资讯');
-
+    function loadFromRemote() {
         HandyHit.feed.load(function(result) {
             if (!result.error) {
-                HandyHit.data.feedEntries(result.feed.entries);
-                isLoaded(true);
-                loadPanelVisible(false);
+                remoteFeedEntries = result.feed.entries;
+                for (var i = 0; i < remoteFeedEntries.length; i++) {
+                    HandyHit.data.feedEntrySource.store().insert(remoteFeedEntries[i]);
+                }
             }
         });
     }
 
     function navigateDetail(entry) {
-        var id = HandyHit.data.feedEntries.indexOf(entry);
-        HandyHit.app.navigate('infoDetail/' + id);
+        HandyHit.app.navigate('infoDetail/' + entry.publishedDate);
     }
 
     var viewModel = {
         // Put the binding properties here
-        isLoaded: isLoaded,
-        loadPanelVisible: loadPanelVisible,
-        loadPanelMessage: loadPanelMessage,
-        feedEntries: HandyHit.data.feedEntries,
+        autoPagingEnabled: autoPagingEnabled,
+        showNextButton: showNextButton,
+        pullRefreshEnabled: pullRefreshEnabled,
+        pullingDownText: pullingDownText,
+        pulledDownText: pulledDownText,
+        pullRefreshAction: loadFromRemote,
+        feedEntrySource: HandyHit.data.feedEntrySource,
         viewShowing: function() {
             if (!HandyHit.util.isConnected()) {
                 DevExpress.ui.notify('无网络连接', 'warning', 3000);
-            } else {
-                loadData();
             }
         },
         navigateDetail: navigateDetail
